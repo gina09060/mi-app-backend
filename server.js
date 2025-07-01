b58eb753af9d0ccab72da19f614b0a6d3366bf1d
Radi090 <espinalradilia@gmail.com>
2025-07-01 16:18:16 -0400

    Actualizaciones

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const multer = require('multer');
 const authRoutes = require('./routers/authRoutes'); // ya usas authRoutes
 require('dotenv').config();
 
-
 const app = express();
-const port = 8000;
+
+// Usar el puerto de la variable de entorno `PORT`, o por defecto el 8000 para desarrollo
+const port = process.env.PORT || 8000;  // En producción, usará el puerto asignado por el entorno
 
 app.use(cors());
 app.use(express.json());
