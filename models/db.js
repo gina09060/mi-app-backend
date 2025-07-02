acc6068342f58a23c37901c2ff17971bac3e385c
Radi090 <espinalradilia@gmail.com>
2025-07-02 00:22:39 -0400

    Ajustes base

--- models/db.js
+++ models/db.js
@@ -5,9 +5,9 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3306,  // El puerto 3306 es el por defecto para MySQL
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: true  // Asegura que la conexión es segura
   }
 });
 
