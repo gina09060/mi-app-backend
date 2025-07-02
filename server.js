463b24cacbe88e6818794db184ed8d7ce8c4746a
gina09060 <sc.radilia.cerdano.e@upds.net.bo>
2025-07-02 02:01:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const authRoutes = require('./routers/authRoutes'); // ya usas authRoutes
 require('dotenv').config();
 
 const app = express();
+const frontendUrl = "https://admirable-entremet-0475ae.netlify.app";  // URL de tu frontend
 
+app.use(cors({
+  origin: frontendUrl,  // Solo permitir solicitudes desde tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 // Usar el puerto de la variable de entorno `PORT`, o por defecto el 8000 para desarrollo
 const port = process.env.PORT || 8000;  // En producción, usará el puerto asignado por el entorno
 
